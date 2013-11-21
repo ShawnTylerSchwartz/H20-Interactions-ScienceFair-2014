60f15d4ceffda2340eb47803a15aae4e0f2c0024
Shawn Schwartz <Shawn@Shawns-iMac.local>
2013-11-20 23:46:09 -0800

    Adds arrays, coords, and random code in main method.

--- H2OInteractionsJPL/src/h2ointeractionsjpl/H2OInteractionsJPL.java
+++ H2OInteractionsJPL/src/h2ointeractionsjpl/H2OInteractionsJPL.java
@@ -1,5 +1,7 @@
 package h2ointeractionsjpl;
 
+import java.util.Random;
+
 /**
  * @author Shawn Tyler Schwartz
  * @school High Tech Los Angeles
@@ -15,8 +17,51 @@ public class H2OInteractionsJPL {
      * @param args the command line arguments
      */
     
+    //Coordinate Arrays
+    public static double initialLeftHydrogenCoord[];
+    public static double initialOxygenCoord[];
+    public static double initialRightHydrogenCoord[];
+    public static double initialMoleculeCoordArray[][];
+    
+    public static double finalLeftHydrogenCoord[];
+    public static double finalOxygenCoord[];
+    public static double finalRightHydrogenCoord[];
+    public static double finalMoleculeCoordArray[][];
+    
+    //Coordinate Values
+    public static double initialLeftHydrogenXPos;
+    public static double initialLeftHydrogenYPos;
+    
+    public static double initialOxygenXPos;
+    public static double initialOxygenYPos;
+    
+    public static double initialRightHydrogenXPos;
+    public static double initialRightHydrogenYPos;
+    
+    
+    public static double finalLeftHydrogenXPos;
+    public static double finalLeftHydrogenYPos;
+    
+    public static double finalOxygenXPos;
+    public static double finalOxygenYPos;
+    
+    public static double finalRightHydrogenXPos;
+    public static double finalRighyHydrogenYPos;
+    
+    //Rotational Flux Constants
+    public static double generatedRotationAngle; //radians
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        double start = 0.0;
+        double end = Math.toRadians(360.0);
+        double randomMolONE = new Random().nextDouble();
+        generatedRotationAngle = start + (randomMolONE * (end - start));
+        sopl("\n\nRandom Angle Molecule One: " + generatedRotationAngle);
+
+        rotationAboutXYZMatrix(randomThetaMolONE, randomThetaMolTWO); //(theta)
     }
     
 }
