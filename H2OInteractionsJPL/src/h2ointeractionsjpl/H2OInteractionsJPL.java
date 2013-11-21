7d7fe5983378e26185e800860c6b258727e376f7
Shawn Schwartz <Shawn@Shawns-iMac.local>
2013-11-20 22:53:48 -0800

    Adds meta data info to main.java file.

--- H2OInteractionsJPL/src/h2ointeractionsjpl/H2OInteractionsJPL.java
+++ H2OInteractionsJPL/src/h2ointeractionsjpl/H2OInteractionsJPL.java
@@ -1,20 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package h2ointeractionsjpl;
 
 /**
- *
- * @author Shawn
- */
+ * @author Shawn Tyler Schwartz
+ * @school High Tech Los Angeles
+ * @advisors Dr. William N. Warner, Wun Chiou, & Daniel Perahya
+ * @location Jet Propulsion Laboratory, Pasadena CA (JPL)
+ * @fileName "H2OInteractionsJPL.java" (Main Class)
+ * @version 2.0 - 2014 LA County Science Fair Edition
+ **/
+
 public class H2OInteractionsJPL {
 
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
